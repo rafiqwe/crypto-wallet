f68d9b5ab389c87101d7ad2c782b5bbfe76d0b61
rafiqwe <rmlmanik502@gmail.com>
2025-12-18 21:40:03 +0600

    Updated: animation gsap

--- app/components/Hero/Hero.tsx
+++ app/components/Hero/Hero.tsx
@@ -1,12 +1,40 @@
-import React from "react";
+"use client";
+
 import TopHero from "./TopHero";
 import BottomHero from "./BottomHero";
-
+import { useRef } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { create } from "domain";
+gsap.registerPlugin(ScrollTrigger);
 const Hero = () => {
+  const HeroTextRef = useRef<HTMLHeadingElement>(null);
+  const ImagesRef = useRef<HTMLDivElement>(null);
+  const TopRef = useRef<HTMLDivElement>(null);
+  const BottomRef = useRef<HTMLDivElement>(null);
+
+  useGSAP(() => {
+    ScrollTrigger.create({
+      trigger: TopRef.current,
+      start: "top top",
+      end: "bottom top",
+      pin: true,
+      pinSpacing: false,
+      markers: true,
+      scrub: true,
+      animation: gsap.to(HeroTextRef.current, {
+        opacity: 0,
+        scale: 0.4,
+        duration: 3,
+        ease: "power3.out",
+      }),
+    });
+  });
   return (
     <div className="w-full">
-      <TopHero />
-      <BottomHero />
+      <TopHero TopRef={TopRef} HeroTextRef={HeroTextRef} />
+      <BottomHero BottomRef={BottomRef} ImagesRef={ImagesRef} />
     </div>
   );
 };

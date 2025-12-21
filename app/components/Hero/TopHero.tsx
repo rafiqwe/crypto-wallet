f6f4e8d559f396c0e158b3cfddc868cf000179ea
rafiqwe <rmlmanik502@gmail.com>
2025-12-21 12:49:25 +0600

    Updated Top hero component

--- app/components/Hero/TopHero.tsx
+++ app/components/Hero/TopHero.tsx
@@ -1,12 +1,18 @@
 import Image from "next/image";
 import hero from "@/public/images/hero-bg-image.png";
 
-const TopHero = () => {
+const TopHero = ({
+  HeroTextRef,
+  TopRef,
+}: {
+  HeroTextRef: React.RefObject<HTMLHeadingElement | null>;
+  TopRef: React.RefObject<HTMLDivElement | null>;
+}) => {
   return (
-    <div className="relative">
+    <div ref={TopRef} className="relative">
       <div className="h-[80vh] pt-10">
-        <div className="flex mx-auto items-center justify-center h-full w-2xl text-center">
-          <h1 className="text-7xl text-black/80 font-bold">
+        <div className="flex mx-auto items-center justify-center h-full w-1/2 text-center">
+          <h1 ref={HeroTextRef} className="text-7xl text-black/80 font-bold">
             Reimagine How You Interact With Bitcoin
           </h1>
         </div>

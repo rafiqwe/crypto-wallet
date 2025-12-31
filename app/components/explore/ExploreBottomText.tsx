713e432cc86538c53be929ecfb533939db85c8e8
rafiqwe <rmlmanik502@gmail.com>
2025-12-31 23:47:01 +0600

    Created: explore bottom text components

---
+++ app/components/explore/ExploreBottomText.tsx
@@ -0,0 +1,15 @@
+const ExploreBottomText = () => {
+  return (
+    <div className=" relative w-full flex items-center justify-center h-full px-10">
+      <h3 className="text-5xl z-10 text-center w-1/2 font-bold text-white">
+        Explore, create, and trade seamlessly{" "}
+        <span className="text-gray-300 font-sans">
+          in the Bitcoin ecosystem.
+        </span>
+      </h3>
+      <div className="h-20 w-70 md:w-100  md:top-1/3 top-1/2 bg-amber-500 blur-3xl absolute left-1/2 -translate-x-28 "></div>
+    </div>
+  );
+};
+
+export default ExploreBottomText;

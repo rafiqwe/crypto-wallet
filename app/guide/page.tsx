d495f365c7a358f80636f29a35e6162699ffb02e
rafiqwe <rmlmanik502@gmail.com>
2026-01-13 20:53:55 +0600

    Created: guide page

---
+++ app/guide/page.tsx
@@ -0,0 +1,13 @@
+import Blogs from "../components/guide/Blogs";
+import TopGuideText from "../components/guide/TopGuideText";
+
+const GuidePage = () => {
+  return (
+    <div className="pt-30 px-7">
+      <TopGuideText />
+      <Blogs/>
+    </div>
+  );
+};
+
+export default GuidePage;

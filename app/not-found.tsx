d5f63f5756a6b7f4f324366dc3d0bc18a8ee0e3d
rafiqwe <rmlmanik502@gmail.com>
2026-01-15 00:50:13 +0600

    Created: not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+import NotFoundAndThanksText from "./components/UI/NotFound&ThanksText";
+
+const NotFound = () => {
+  return <NotFoundAndThanksText title="404 Not Found" />;
+};
+
+export default NotFound;

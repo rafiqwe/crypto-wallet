022910a2576daad6503b33acbbbe8d19a0220bb6
rafiqwe <rmlmanik502@gmail.com>
2026-01-20 22:33:43 +0600

    Created: thanks you page

---
+++ app/thank-you/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import NotFoundAndThanksText from "../components/UI/NotFound&ThanksText";
+
+const page = () => {
+  return <NotFoundAndThanksText title="Thank You! your now a subscriber" />;
+};
+
+export default page;
